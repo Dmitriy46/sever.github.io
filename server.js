1fe3f358da046727218accb084ca8b6b865b8693
Dmitriy46 <fcsm46@list.ru>
2025-06-07 14:07:24 +0300

    1

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+
+app.use(express.static('public')); // Теперь сервер будет отдавать файлы из папки public
 app.use(bodyParser.json());
 
 // Имитация базы данных
